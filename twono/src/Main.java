15a515aa10435ded7ad9cb4946fcfabadfd4f9ce
Benedict juma <94790520+Benedict2002@users.noreply.github.com>
2024-01-24 22:05:10 +0300

    Add files via upload

---
+++ twono/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class twono {
+    public static void main(String[] args) {
+        Scanner num = new Scanner(System.in);
+        int a = num.nextInt();
+        int b = num.nextInt();
+
+        System.out.println(a+" x "+b+" = "+a*b);
+
+
+    }
+
+}
